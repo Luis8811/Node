4028432082570e14325b18cadd4894db2f7568ad
Luis Manuel Suárez González <lsuarezg8811@gmail.com>
2018-04-24 00:36:23 +0200

    Minors changes in views and controllers

--- proyectomean/app_server/controllers/main.js
+++ proyectomean/app_server/controllers/main.js
@@ -1,4 +1,4 @@
 //controlador principal de la app
 module.exports.load = function(req, res){
-    res.render('index', {title: 'Contenido de la página de inicio de la app.'});
+    res.render('index', {title: 'Dulcelandia', welcome_message: 'Ordenar los mejores dulces nunca había sido tan sencillo!'});
    };
--- proyectomean/app_server/controllers/orders.js
+++ proyectomean/app_server/controllers/orders.js
@@ -1,5 +1,5 @@
 //Controlador de los pedidos
 
 module.exports.orders = function(req, res){
- res.render('index', {title: 'Orders'});
+ res.render('orders', {subtitle: 'Registrar pedidos', cantidad:'40', clientes:'20'});
 };
